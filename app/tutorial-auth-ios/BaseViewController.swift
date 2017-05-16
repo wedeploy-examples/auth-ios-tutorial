f0a6326153b04fe120418328b41d32e570a32c32
Victor Galan <victor.galan@liferay.com>
2017-05-16 14:31:11 +0200

    Initial project

---
+++ app/tutorial-auth-ios/BaseViewController.swift
@@ -0,0 +1,43 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+	let authUrl = "http://auth.boilerplate-auth.wedeploy.io"
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap))
+		view.addGestureRecognizer(tapGesture)
+	}
+	
+	func handleScreenTap() {
+		view.endEditing(true)
+	}
+	
+	func showAlert(with title: String, message: String) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+		alert.addAction(action)
+		
+		present(alert, animated: true, completion: nil)
+	}
+	
+	@IBAction func goBack() {
+		self.navigationController?.popViewController(animated: true)
+	}
+}
